2d8e88033e41d3972632f620f75ee69584b0f704
yasmeen saleh <120665573+yasmeen865@users.noreply.github.com>
2022-12-19 20:35:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Program
+    {
+       
+        
+            public static int Factors(int n2)
+            {
+                int sum = 0;
+                for (int i = 1; i < n2; i++)
+                {
+                    if (n2 % i == 0)
+                    {
+                        sum = sum + i;
+                    }
+                }
+
+                return sum;
+            }
+
+
+            public static void Main(string[] args)
+            {
+                int n2, n1;
+
+                Console.WriteLine("enter frist number");
+                n1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter provide number");
+                n2 = int.Parse(Console.ReadLine());
+
+                for (int i = n1; i <= n2; i++)
+                {
+                    int result = Factors(i);
+                    if (result == i)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+    }
+}
